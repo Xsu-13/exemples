92f0eb8d1a0123cb827bc491e49dbdac66b5f123
Xsu-13 <73441321+Xsu-13@users.noreply.github.com>
2021-07-01 12:19:30 +0300

    Add files via upload

---
+++ scripts01/GameEnviroment.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+public sealed class GameEnviroment 
+{
+    private static GameEnviroment instance;
+    private List<GameObject> checkpoints = new List<GameObject>();
+    public List<GameObject> Chackpoints = new List<GameObject>();
+
+    public static GameEnviroment Singleton
+    {
+        get
+        {
+            if (instance==null)
+            {
+                instance = new GameEnviroment();
+                instance.Chackpoints.AddRange(GameObject.FindGameObjectsWithTag("Checkpoint"));
+
+                instance.checkpoints = instance.checkpoints.OrderBy(waypoints => waypoints.name).ToList();
+            }
+            return instance;
+        }
+    }
+    
+}
